a166eee7a619d6edc243c07faa4a10efb3c92367
Jonathan Alexis Montenegro <jonathanalexismontenegro97@gmail.com>
2021-03-07 20:02:01 -0300

    Script de la vida del enemigo

---
+++ Example Goty/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int lifes = 5;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerDamage"))
+        {
+            checkLife();
+        }
+    }
+
+    void checkLife()
+    {
+        lifes--;
+
+        if (lifes == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Example Goty/Assets/Scripts/TowerAttack.cs
+++ Example Goty/Assets/Scripts/TowerAttack.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class TowerAttack : MonoBehaviour
 {
-
-
     public float radio=1f;
     public float cdAttack = 1.5f;
     public float currectCdAttack = 0;
     public float distance = 1f;
-    public GameObject arrow;
     
-    // Start is called before the first frame update
     void Start()
     {
         currectCdAttack = 0;
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         currectCdAttack -= Time.deltaTime;
@@ -33,12 +27,8 @@ public class TowerAttack : MonoBehaviour
         {
             if (raycast.collider.CompareTag("Enemy") && currectCdAttack<0)
             {
-                
                 currectCdAttack = cdAttack;
             }
         }
     }
-
-
-   
 }
