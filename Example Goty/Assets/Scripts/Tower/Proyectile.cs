3d2c5b40211523db8013441243ead36f46a3aee5
Jonathan Alexis Montenegro <jonathanalexismontenegro97@gmail.com>
2021-03-15 17:41:39 -0300

    Proyectil parabolico y carpetas de scrips

---
+++ Example Goty/Assets/Scripts/Tower/Proyectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectile : MonoBehaviour
+{
+    public Rigidbody2D bulletPrefab;
+    public Transform target;
+    public float time = 1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, time);
+        Vector3 Vo = CalculateVelocity(target.position, transform.position, time);
+
+        bulletPrefab.velocity = Vo;
+    }
+
+    private void Update()
+    {
+        Vector2 v = bulletPrefab.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void TowerTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
+    {
+        Vector3 distance = target - origin;
+        Vector3 distanceXZ = distance;
+        distanceXZ.y = 0f;
+
+        float Sy = distance.y;
+        float Sxz = distanceXZ.magnitude;
+
+        float Vxz = Sxz / time;
+        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+        Vector3 result = distanceXZ.normalized;
+
+        result *= Vxz;
+        result.y = Vy;
+
+        return result;
+    }
+}
