42508524e3bae89ea9a8c54ee0878f87b048d83d
Jonathan Alexis Montenegro <jonathanalexismontenegro97@gmail.com>
2021-04-02 12:50:25 -0300

    Pause

---
+++ Example Goty/Assets/Scripts/CanvasPanelOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasPanelOptions : MonoBehaviour
+{
+    public GameObject panelOptions;
+
+    public void PanelOptions()
+    {
+        Time.timeScale = 0;
+        panelOptions.SetActive(true);
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1;
+        panelOptions.SetActive(false);
+    }
+}
